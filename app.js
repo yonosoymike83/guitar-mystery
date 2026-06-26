01fffa4f490236965de3d4d80c8cf7e7fc0421e9
Mike <mike.knoppix@gmail.com>
2026-06-27 00:21:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function buildFretboard() {
     fretboard.innerHTML = "";
     fretLabels.innerHTML = "";
 
-    const strings = ["E", "A", "D", "G", "B", "e"];
+    const strings = ["E", "A", "D", "G", "B", "E"];
 
     const emptyLabel = document.createElement("div");
     emptyLabel.className = "fret-label";
